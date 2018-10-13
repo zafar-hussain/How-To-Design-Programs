b92860c89b4bd1f91d6f51b2a483f63ef55d66de
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-10-14 01:01:35 +0500

    Signed-off-by: zafar-hussain

---
+++ drawings.bak
@@ -0,0 +1,13 @@
+#lang htdp-beginner
+
+(require htdp/draw)
+(require 2htdp/universe)
+(require 2htdp/image)
+
+;;(define-struct posn [x y])
+
+(start 300 300)
+(draw-solid-line (make-posn 0 0) (make-posn 5 5) 'red)
+(draw-solid-rect (make-posn 20 10) 50 200 'blue)
+(draw-circle (make-posn 200 10) 50 'green)
+(draw-solid-disk (make-posn 200 10) 50 'purple)
--- drawings.rkt
+++ drawings.rkt
@@ -1,13 +1,27 @@
-#lang htdp-beginner
-
-(require htdp/draw)
-(require 2htdp/universe)
+#lang racket
 (require 2htdp/image)
 
-;;(define-struct posn [x y])
+;Exercise 6.6.1 Provide a structure definition and a data definition for representing colored circles.
+; A circle is characterized by three pieces of information: its center, its radius, and the
+;color of its perimeter.
 
-(start 300 300)
-(draw-solid-line (make-posn 0 0) (make-posn 5 5) 'red)
-(draw-solid-rect (make-posn 20 10) 50 200 'blue)
-(draw-circle (make-posn 200 10) 50 'green)
-(draw-solid-disk (make-posn 200 10) 50 'purple)
+
+;; posn -> make-posn x y ; a point in cartecian cordinate system
+;; posn -> number number
+(define-struct posn [x y])
+;; examples of posn
+(define p00 (make-posn 0 0))
+(define p01 (make-posn 0 1))
+(define p10 (make-posn 1 0))
+(define p11 (make-posn 1 1))
+
+;; aCircle -> make-cirle center radius color
+;; aCircle ->            posn   number symbol
+
+(define-struct aCircle [center radius color])
+
+
+
+; The first is a posn structure, the second a number, and the third a (color) symbol.
+
+;Develop the template fun-for-circle, which outlines a function that consumes circles. Its result is undetermined.
