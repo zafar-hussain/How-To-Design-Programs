7ed4eb9dafe24c804ea0258ced88507e5c6cbb1b
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-10-14 16:43:32 +0500

    Signed-off-by: zafar-hussain

--- drawings.rkt
+++ drawings.rkt
@@ -1,27 +1,42 @@
-#lang racket
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname drawings) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;#lang racket
 (require 2htdp/image)
 
 ;Exercise 6.6.1 Provide a structure definition and a data definition for representing colored circles.
 ; A circle is characterized by three pieces of information: its center, its radius, and the
 ;color of its perimeter.
 
-
-;; posn -> make-posn x y ; a point in cartecian cordinate system
-;; posn -> number number
-(define-struct posn [x y])
-;; examples of posn
-(define p00 (make-posn 0 0))
-(define p01 (make-posn 0 1))
-(define p10 (make-posn 1 0))
-(define p11 (make-posn 1 1))
+;; as circle needs a aPosn structure for center
+;; aPosn -> make-aPosn x y ; a point in cartecian cordinate system
+;; aPosn -> number number
+(define-struct aPosn [x y])
+;; examples of aPosn
+(define p00 (make-aPosn 0 0))
+(define p01 (make-aPosn 0 1))
+(define p10 (make-aPosn 1 0))
+(define p11 (make-aPosn 1 1))
 
 ;; aCircle -> make-cirle center radius color
-;; aCircle ->            posn   number symbol
+;; aCircle ->            aPosn   number symbol
+; The first is a aPosn structure, the second a number, and the third a (color) symbol.
 
 (define-struct aCircle [center radius color])
-
-
-
-; The first is a posn structure, the second a number, and the third a (color) symbol.
+;; examples for circle
+(define c00 (make-aCircle p00 10 'red))
+(define c01 (make-aCircle p01 10 'green))
 
 ;Develop the template fun-for-circle, which outlines a function that consumes circles. Its result is undetermined.
+;; function template
+#;
+(define (fn-for-aCircle c)
+  ... (aCircle-center (aPosn-x c))
+  ... (aCircle-center (aPosn-y c))
+  ... (aCircle-radius c)
+  ... (aCircle-color c)
+  )
+
+
+
+
