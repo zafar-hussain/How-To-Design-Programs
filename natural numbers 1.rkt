a0d5537f1a79424fb85438abfbb8a19f448b7d66
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-10-30 14:42:58 +0500

    tabulate-f

--- natural numbers 1.rkt
+++ natural numbers 1.rkt
@@ -1,52 +1,72 @@
-#lang racket
-(require test-engine/racket-tests)
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |natural numbers 1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;#lang racket
+;;(require test - engine/racket - tests)
 
-;;;   Exercise 11.2.1 Generalize hellos to repeat, which consumes a natural number n and a symbol
-;;; and produces a list with n occurrences of the symbol.
+; ; ; Exercise 11.2.1 Generalize hellos to repeat, which consumes a natural number n and a symbol
+; ; ; and produces a list with n occurrences of the symbol.
+; ; symbol - n - times:n (number)s (symbol) -  > listOfSymbols
+; ; returns a listOfSymbols, with symbol repeated n times;
 
-;; symbol-n-times : n (number) s (symbol) -> listOfSymbols
-;; returns a listOfSymbols, with symbol repeated n times
-;; Examples
+; Examples
+
 (check-expect (symbol-n-times 0 'aSymbol) empty) ; basecase
-(check-expect (symbol-n-times 1 'aSymbol) (cons 'aSymbol empty))
-(check-expect (symbol-n-times 3 'test) (list 'test 'test 'test))
+(check-expect (symbol-n-times 1 'aSymbol)(cons 'aSymbol empty))
+(check-expect (symbol-n-times 3 'test)(list 'test 'test 'test))
 
 ; (define (symbol-n-times n los) empty) ; stub
 
 ;; template
 #;
 (define (fn-for-los n los)
-(cond
+  (cond
     [(zero? n) ...]
     [else
-    (...
-    ... (first los)
-    ... (fn-for-los (sub1 n) (rest los) )
+     (...
+      ... (first los)
+      ... (fn-for-los (sub1 n) (rest los) )
 
-    )]
+      )]
 
-))
+    ))
 
 (define (symbol-n-times n s)
-(cond
+  (cond
     [(zero? n) empty]
     [else 
-    (cons 
-        s 
-        (symbol-n-times (sub1 n) s )
-    )]
-))
+     (cons 
+      s 
+      (symbol-n-times (sub1 n) s )
+      )]
+    ))
 
-(test)
+
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++
 ;;; Exercise 11.2.2 Develop the function tabulate-f, which tabulates the values of
 ;;; ;; f : number → number
-;;; (define (f x)
-;;; (+ (* 3 (* x x))
-;;; (+ (* -6 x)
-;;; -1)))
-;;; for some natural numbers. Specifically, it consumes a natural number n and produces a list of nposns. The first one combines n with (f n), the second one n-1 with (f n-1), etc.
+(define (f x)
+  (+ (* 3 (* x x))
+     (+ (* -6 x)
+        -1)))
+;;; for some natural numbers. Specifically, it consumes a natural number n and produces a list of nposns.
+;;; The first one combines n with (f n), the second one n-1 with (f n-1), etc.
+;; example
+(check-expect (tabulate-f 0) empty) ; basecase
+(check-expect (tabulate-f 1) (cons (make-posn 1 (f 1)) empty))
+(check-expect (tabulate-f 3) (list (make-posn 3 (f 3)) (make-posn 2 (f 2)) (make-posn 1 (f 1))))
 
+;(define (tabulate-f n) empty) ; stub
+
+(define (tabulate-f n)
+  (cond
+    [(zero? n) empty]
+    [else
+     (cons
+      (make-posn n (f n))
+      (tabulate-f (sub1 n))
+      )]
+    ))
 
 ;; +++++++++++++++++++++++++++++++++++++++++++++++
 ;;; Exercise 11.2.3 Develop apply-n. The function consumes a natural number, n. It applies the
@@ -68,7 +88,7 @@
 
 
 
-
+;(test)
 
 
 
