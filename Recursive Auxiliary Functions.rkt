213e9d5df47c9b862d9027ca590c9bce2eaf4552
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-11-04 15:09:31 +0500

    sort-mail

--- Recursive Auxiliary Functions.rkt
+++ Recursive Auxiliary Functions.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |Recursive Auxiliary Functions|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;#lang racket
 ;(require test-engine/racket-tests)
 ;
@@ -37,6 +40,73 @@
     [(>= atomic (first lon)) (cons atomic       lon)]
     [(<  atomic (first lon)) (cons (first lon) (insert atomic (rest lon)))]
     ))
+
+
+
+(define-struct mail (from date message))
+;A mail-message is a structure:
+;(make-mail name n s)
+(define m0 (make-mail "zafar" 0 "hussain"))
+(define m1 (make-mail "aun"   1 "ali"))
+(define m2 (make-mail "taha"  2 "ali"))
+
+#;
+(define (fn-for-mail m)
+  (...
+   ... (mail-name    m)
+   ... (mail-date    m)
+   ... (mail-message m)
+   ))
+
+;; listOfMail
+#;
+(define (fn-for-listOfMail lom)
+  (cond
+    [(empty? lom) ...]
+    [else
+     (...
+      ... (first lom)
+      ... (fn-for-listOfMail (rest lom)))
+     ]))
+
+;Exercise 12.2.1 Develop a program that sorts lists of mail messages by date. Mail structures are
+;defined as follows:
+;(define-struct mail (from date message))
+;A mail-message is a structure:
+;(make-mail name n s)
+;where name is a string, n is a number, and s is a string.
+(check-expect (sort-mail empty) empty) ; basecase
+(check-expect (sort-mail (cons m0 (cons m1 empty))) (cons m0 (cons m1 empty)))
+(check-expect (sort-mail (cons m1 (cons m0 empty))) (cons m0 (cons m1 empty)))
+(check-expect (sort-mail (cons m2 (cons m0 (cons m1 empty)))) (cons m0 (cons m1 (cons m2 empty))))
+
+;(define (sort-mail lom) empty) ; stub
+(define (sort-mail lom)
+  (cond
+    [(empty? lom) empty]
+    [else
+     (mail-insert
+      (first lom)
+      (sort-mail (rest lom)))
+     ]))
+
+;; mail-insert : X loX -> loX
+;; inserts the given mail within the listOfMail in ascending order by date
+(check-expect (mail-insert m0 empty) (cons m0 empty)) ; basecase
+(check-expect (mail-insert m0 (cons m1 empty)) (cons m0 (cons m1 empty)))
+(check-expect (mail-insert m1 (cons m0 empty)) (cons m0 (cons m1 empty)))
+(check-expect (mail-insert m2 (cons m0 (cons m1 empty))) (cons m0 (cons m1 (cons m2 empty))))
+
+;(define (mail-insert m lom) (cons m empty)) ; stub
+(define (mail-insert m lom)
+  (cond
+    [(empty? m lom) (cons m empty)]
+    [(<= m (first lom)) (cons m lom)]
+    [(> m (first lom))  (cons (first lom) (mail-insert m (rest lom)))]
+    ))
+
+;Also develop a program that sorts lists of mail messages by name. To compare two strings
+;alphabetically, use the string < ? primitive
 
 
 ;(test)
